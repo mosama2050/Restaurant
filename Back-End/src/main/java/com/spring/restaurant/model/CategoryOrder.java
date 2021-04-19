a47fb7bfa1e2a98197980abde5e74a15cf10d92a
mosama2050 <37184677+mosama2050@users.noreply.github.com>
2021-04-19 17:44:37 +0200

    Timestamp

--- Back-End/src/main/java/com/spring/restaurant/model/CategoryOrder.java
+++ Back-End/src/main/java/com/spring/restaurant/model/CategoryOrder.java
@@ -1,7 +1,10 @@
 package com.spring.restaurant.model;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
@@ -10,4 +13,13 @@ import javax.persistence.*;
 public class CategoryOrder extends BaseEntity{
     @Column(name = "name")
     private String name;
+
+    @Column(name = "data_create")
+    @CreationTimestamp
+    private Date dataCreate;
+
+    @Column(name = "data_update")
+    @UpdateTimestamp
+    private Date dataupdate;
+
 }

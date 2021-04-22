199fae38b2560d1271e07954568ea8e48ba4b6af
mosama2050 <37184677+mosama2050@users.noreply.github.com>
2021-04-23 01:43:14 +0200

    Model (Country & State) anotation + relation

--- Back-End/src/main/java/com/spring/restaurant/model/Country.java
+++ Back-End/src/main/java/com/spring/restaurant/model/Country.java
@@ -1,6 +1,17 @@
 package com.spring.restaurant.model;
 
-public class Country extends PublicData{
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Set;
 
+@Entity
+@Table(name ="country")
+public class Country extends PublicData{
+    @Column(name = "code")
     private String code; // EG
+
+    @OneToMany(mappedBy = "country")
+    private Set<State> states;
   }

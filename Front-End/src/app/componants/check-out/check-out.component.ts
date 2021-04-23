55ee9df0174eb80733e91ecf17f564a0850ee7cf
mosama2050 <37184677+mosama2050@users.noreply.github.com>
2021-04-23 17:59:01 +0200

    DataForm Validator done   Closes #29

--- Front-End/src/app/componants/check-out/check-out.component.ts
+++ Front-End/src/app/componants/check-out/check-out.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {CartServiceService} from '../../service/cart-service.service';
 import {StateCountryServiceService} from '../../service/state-country-service.service';
 import {Country} from '../../model/country';
 import {State} from '../../model/state';
+import {SpaceValidator} from '../../model/space-validator';
 
 @Component({
   selector: 'app-check-out',
@@ -12,46 +13,59 @@ import {State} from '../../model/state';
 })
 export class CheckOutComponent implements OnInit {
 
-  checkoutParentGroup : FormGroup;
+  checkoutParentGroup: FormGroup;
   totalOrder: number = 0;
   totalPrice: number = 0;
 
-  countries : Country [] = [] ;
-  statesFromPerson : State [] = [] ;
-   statesToPerson : State [] = [] ;
-  constructor(private cart: CartServiceService,private  formChildGroup : FormBuilder
-  ,private  stateContry :StateCountryServiceService
-  ) { }
+  countries: Country [] = [];
+  statesFromPerson: State [] = [];
+  statesToPerson: State [] = [];
+
+  constructor(private cart: CartServiceService, private formChildGroup: FormBuilder
+    , private stateContry: StateCountryServiceService
+  ) {
+  }
 
   ngOnInit(): void {
-    this.myForm()
-    this.getTotals()
-    this.cart.calculateTotals()
-    this.getAllCountries()
+    this.myForm();
+    this.getTotals();
+    this.cart.calculateTotals();
+    this.getAllCountries();
 
 
   }
 
-  getTotals(){
+  getTotals() {
     this.cart.totalOrders.subscribe(
       data => {
-        this.totalOrder = data
+        this.totalOrder = data;
       }
-    )
+    );
     this.cart.totalPrice.subscribe(
       data => {
-        this.totalPrice = data
+        this.totalPrice = data;
       }
-    )
+    );
   }
 
 
   private myForm() {
     this.checkoutParentGroup = this.formChildGroup.group({
-        data : this.formChildGroup.group({
-          fullName: [''],
-          gmail: [''],
-          phone: ['']
+        data: this.formChildGroup.group({
+          fullName: new FormControl('', [
+            Validators.required,
+            SpaceValidator.onlyContainSpace,
+            Validators.minLength(6)]),
+          gmail: new FormControl('', [
+            Validators.required,
+            Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')
+          ]),
+          phone: new FormControl('', [
+            Validators.required,
+            Validators.minLength(11),
+            Validators.maxLength(11),
+            Validators.pattern('^[0-9]*$')
+          ])
         }),
 
         fromPerson: this.formChildGroup.group({
@@ -71,54 +85,70 @@ export class CheckOutComponent implements OnInit {
         })
 
       }
-    )
+    );
 
+  }
+
+  get fullName() { // get -> element from html
+    return this.checkoutParentGroup.get('data.fullName');
+  }
+
+  get email() {
+    return this.checkoutParentGroup.get('data.gmail');
+  }
+
+  get phone() {
+    return this.checkoutParentGroup.get('data.phone');
   }
 
   done() {
-    console.log(this.checkoutParentGroup.get('data.fullName').value)
-    console.log(this.checkoutParentGroup.get('fromPerson').value)
-    console.log(this.checkoutParentGroup.get('toPerson').value)
-    console.log(this.checkoutParentGroup.get('creditCard').value)
-  }
-
-  similarGroup(event: Event) {
-    if((<HTMLInputElement>event.target).checked){
-      this.checkoutParentGroup.controls.toPerson
-        .setValue(this.checkoutParentGroup.controls.fromPerson.value)
-      this.statesToPerson=this.statesFromPerson
+    if (this.checkoutParentGroup.invalid) {
+      this.checkoutParentGroup.markAllAsTouched();
     } else {
-      this.checkoutParentGroup.controls.toPerson.reset()
+      console.log(this.checkoutParentGroup.get('data.fullName').value);
+      console.log(this.checkoutParentGroup.get('fromPerson').value);
+      console.log(this.checkoutParentGroup.get('toPerson').value);
+      console.log(this.checkoutParentGroup.get('creditCard').value);
     }
   }
 
-  getAllCountries(){
-    this.stateContry.getAllCountry().subscribe(
-      data => {
-        this.countries = data
-      }
-    )
+  similarGroup(event: Event) {
+    if ((<HTMLInputElement> event.target).checked) {
+      this.checkoutParentGroup.controls.toPerson
+        .setValue(this.checkoutParentGroup.controls.fromPerson.value);
+      this.statesToPerson = this.statesFromPerson;
+    } else {
+      this.checkoutParentGroup.controls.toPerson.reset();
+    }
   }
 
- // getAllStates() {
- //    this.stateContry.getAllStates().subscribe(
- //      data => {
- //        this.states = data
- //      }
- //    )
- //  }
-  getStatesByCode(typeform : string){
-    const code = this.checkoutParentGroup.get(`${typeform}.country`).value
+  getAllCountries() {
+    this.stateContry.getAllCountry().subscribe(
+      data => {
+        this.countries = data;
+      }
+    );
+  }
+
+  // getAllStates() {
+  //    this.stateContry.getAllStates().subscribe(
+  //      data => {
+  //        this.states = data
+  //      }
+  //    )
+  //  }
+  getStatesByCode(typeform: string) {
+    const code = this.checkoutParentGroup.get(`${typeform}.country`).value;
 
     this.stateContry.getStatesByCode(code).subscribe(
-      data =>{
-        if(typeform==="fromPerson") {
-          this.statesFromPerson = data
-        }else{
-          this.statesToPerson = data
+      data => {
+        if (typeform === 'fromPerson') {
+          this.statesFromPerson = data;
+        } else {
+          this.statesToPerson = data;
         }
-        this.checkoutParentGroup.get(`${typeform}.state`).setValue(data[0])
+        this.checkoutParentGroup.get(`${typeform}.state`).setValue(data[0]);
       }
-    )
+    );
   }
 }

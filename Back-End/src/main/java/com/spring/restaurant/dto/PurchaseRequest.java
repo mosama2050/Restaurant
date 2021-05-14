9a6e104203ef54b0516fc9919a5b9d23524bf91c
mosama2050 <37184677+mosama2050@users.noreply.github.com>
2021-05-14 23:59:47 +0200

    Purchase

--- Back-End/src/main/java/com/spring/restaurant/dto/PurchaseRequest.java
+++ Back-End/src/main/java/com/spring/restaurant/dto/PurchaseRequest.java
@@ -1,23 +1,19 @@
 package com.spring.restaurant.dto;
 
-import com.spring.restaurant.model.Client;
 import com.spring.restaurant.model.Address;
-
+import com.spring.restaurant.model.Client;
 import com.spring.restaurant.model.Item;
 import com.spring.restaurant.model.RequestOrder;
 import lombok.Data;
 
-
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Data
 public class PurchaseRequest {
 
     private Client client;
     private RequestOrder requestOrder;
-    private Set<Item> items = new HashSet<>();
+    private List<Item> items  ;
     private Address fromAddress;
     private Address toAddress;
 

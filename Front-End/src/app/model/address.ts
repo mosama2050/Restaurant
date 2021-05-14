2ec382c8812161f4e4a26f9966ab598c2431d510
mosama2050 <37184677+mosama2050@users.noreply.github.com>
2021-05-15 00:28:46 +0200

    Angular Create Models

---
+++ Front-End/src/app/model/address.ts
@@ -0,0 +1,5 @@
+export class Address {
+  country: string;
+  state: string;
+  zipCode: string;
+}
---
+++ Front-End/src/app/model/client.ts
@@ -0,0 +1,5 @@
+export class Client {
+  name: string;
+  email: string;
+  phoneNumber: string;
+}

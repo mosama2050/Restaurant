27b6568bcd48975e64c1926914fc4d8fd9d79a1c
mosama2050 <37184677+mosama2050@users.noreply.github.com>
2021-05-23 01:21:46 +0200

    SessionCreationPolicy STATELESS

--- Back-End/src/main/java/com/spring/restaurant/config/springsecurity/SpringSecurityConfig.java
+++ Back-End/src/main/java/com/spring/restaurant/config/springsecurity/SpringSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -28,6 +29,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // stop session for spring
+                .and()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
                 .antMatchers("/login").permitAll()
